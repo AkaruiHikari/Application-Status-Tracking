70c8be195e58281fdcda84816d36890669e9bfbf
JhonnBretana <jhonnbretz@gmail.com>
2025-04-28 19:21:46 +0800

    1

--- Tracker/php/login.php
+++ Tracker/php/login.php
@@ -1,7 +1,11 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *"); // allow all origins
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); // allow these methods
+header("Access-Control-Allow-Headers: Content-Type"); // allow Content-Type headers
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 include 'db_connect.php';
 
