2732b351beb6c7bfacae35d9abf5f1b0eee909be
JhonnBretana <jhonnbretz@gmail.com>
2025-04-28 18:34:37 +0800

    route

--- Tracker/src/App.jsx
+++ Tracker/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import StartPage from './Pages/Start'
 import Applicants from './Pages/Applicants'
 import Notification from './Pages/Notification'
 import AdminSide from './Pages/AdminSide'
@@ -13,11 +12,10 @@ export default function App() {
   return (
 
     <Routes>
-      <Route path="/" element={<StartPage/>}/> 
       <Route path="/applicants" element={<Applicants/>}/> 
       <Route path="/notification" element={<Notification/>}/>
       <Route path="/admin" element={<AdminSide/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/" element={<LoginPage/>}/>
     </Routes>
 
 
