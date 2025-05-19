69f6170be3a7b334e6b18e5ed617fcbdfb852043
AndreiRosca <ireachpvp@gmail.com>
2025-05-19 14:14:55 +0800

    delete fix

--- Tracker/php/delete_applicant.php
+++ Tracker/php/delete_applicant.php
@@ -1,7 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 
 include 'db_connect.php';
 
